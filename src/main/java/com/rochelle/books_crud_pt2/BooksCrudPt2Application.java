71d4dc8d3a2f4ff79a7614dde24572e344227e04
RochelleLBeckford <r.beckford@me.com>
2023-02-16 00:16:58 -0500

    initial commit

---
+++ src/main/java/com/rochelle/books_crud_pt2/BooksCrudPt2Application.java
@@ -0,0 +1,13 @@
+package com.rochelle.books_crud_pt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksCrudPt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksCrudPt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rochelle/books_crud_pt2/BooksCrudPt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rochelle.books_crud_pt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksCrudPt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
